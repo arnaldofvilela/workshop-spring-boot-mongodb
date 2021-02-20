744c960d45be003ac105d01456a840e8924a5632
Arnaldo Neto <vilela-neto@hotmail.com>
2021-02-20 20:14:43 -0300

    Consulta simples com query methods

--- src/main/java/com/arnaldoneto/workshopmongo/services/PostService.java
+++ src/main/java/com/arnaldoneto/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.arnaldoneto.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arnaldoneto.workshopmongo.domain.Post;
-import com.arnaldoneto.workshopmongo.domain.User;
 import com.arnaldoneto.workshopmongo.repository.PostRepository;
 import com.arnaldoneto.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,4 +21,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
